feaae964b5131dd1df5964878abb89ecc4bd0caf
ledoctah <matheusferr2015@gmail.com>
2020-03-29 00:10:26 -0300

    Home + product filter + improved product listing

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,6 +1,9 @@
 const db = require('../../config/db');
 
 module.exports = {
+    all() {
+        return db.query(`SELECT * FROM products ORDER BY updated_at DESC`);
+    },
     create(data) {
     
         const query = `INSERT INTO products (category_id, user_id, name, description, old_price, price, quantity, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`
@@ -57,5 +60,23 @@ module.exports = {
     },
     files(id) {
         return db.query(`SELECT * FROM files WHERE product_id = $1`, [id]);
+    },
+    search(params) {
+        const { filter, category } = params
+
+        let query = '',
+            filterQuery = `WHERE`;
+
+        if(category) {
+            filterQuery = `${filterQuery} products.category_id = ${category} AND`;
+        }
+
+        filterQuery = `${filterQuery} (products.name ILIKE '%${filter}%' OR products.description ILIKE '%${filter}%')`;
+
+        query = `SELECT products.*, categories.name AS category_name FROM products
+        LEFT JOIN categories ON (categories.id = products.category_id)
+        ${filterQuery}`;
+
+        return db.query(query);
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@ const express = require('express');
 const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const ProductController = require('./app/controllers/ProductController');
+const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController');
 
-routes.get('/', function(req, res) {
-    return res.render('layout.njk');
-});
+//Home
+routes.get('/', HomeController.index);
 
+//Search
+routes.get('/products/search', SearchController.index);
+
+//Products
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show);
 routes.get('/products/:id/edit', ProductController.edit);
@@ -15,7 +20,7 @@ routes.post('/products', multer.array("photos", 6), ProductController.post);
 routes.put('/products', multer.array("photos", 6), ProductController.put);
 routes.delete('/products', ProductController.delete);
 
-//alias
+//Alias
 routes.get('/ads/create', function(req, res) {
     return res.redirect('/products/create');
 });
