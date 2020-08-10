183834f3f9d0ee57e471e21bc9cc80769610b2a4
ledoctah <matheusferr2015@gmail.com>
2020-08-10 18:00:10 -0300

    Added Base model + restructuring started

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,24 +3,13 @@ const { hash } = require('bcryptjs');
 const fs = require('fs');
 
 const Products = require('../models/Product');
+const Base = require('./Base');
+
+Base.init({ table: 'users' });
+
 module.exports = {
-    async findOne(filters) {
-        let query = "SELECT * FROM users";
-
-        Object.keys(filters).map(key => {
-            query = `${query}
-            ${key}`
-
-            Object.keys(filters[key]).map(field => {
-                query = `${query} ${field} = '${filters[key][field]}'`
-            });
-        });
-
-        const results = await db.query(query);
-
-        return results.rows[0];
-    },
-    async create(data) {
+    ...Base,
+    /*async create(data) {
         try{
             const query = `INSERT INTO users (name, email, password, cpf_cnpj, cep, address) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
 
@@ -42,7 +31,7 @@ module.exports = {
         } catch(err) {
             console.log(err);
         }
-    },
+    },*/
     async update(id, fields) {
         let query = "UPDATE users SET";
 
